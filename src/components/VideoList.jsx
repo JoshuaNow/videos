1e9d112a352adcaa978ff1921a58dc75a468a3fd
Joshua Lopez <jrenelopez92@gmail.com>
2021-09-02 16:25:31 -0500

    displaying  description and title

---
+++ src/components/VideoList.jsx
@@ -0,0 +1,14 @@
+import { div } from "prelude-ls";
+import React from "react";
+import VideoItem from "./VideoItem";
+
+const VideoList = ({ videos, onVideoSelect }) => {
+  //props.videos
+
+  const renderedList = videos.map((video) => (
+    <VideoItem onVideoSelect={onVideoSelect} video={video} />
+  ));
+  return <div className="ui relaxed divided list"> {renderedList}</div>;
+};
+
+export default VideoList;
